8edc502cbafecc321b4f3f3333bf536ec042c2be
ashan <indrajee79@gmail.com>
2022-12-19 19:27:04 +0530

    Js stuff

--- script.js
+++ script.js
@@ -23,3 +23,32 @@ const weekdays = [
     'Saturday',
 ];
 
+const giveaway = document.querySelector('.givaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format');
+
+let tempDate =  new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = document.querySelectorAll('.deadline-format h4')
+let tempDay = tempDate.getDate();
+//months are zero index based;
+const futureDate = new Date(tempYear, tempMonth,tempDay + 10, 11, 30, 0);
+
+// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month =  futureDate.getMonth();
+month = months[month];
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours} ${minutes}am`;
+
+const futureTime = futureDate.getTime();
+function getRemaindingTime() {
+    const today = new Date().getTime();
+
+    const t = futureTime - today;
+}
