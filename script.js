e5fcdde56e8ede04c4590826863aed56197566a2
ashan <indrajee79@gmail.com>
2022-12-20 08:05:18 +0530

    continuing more js code

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ const weekdays = [
 
 const giveaway = document.querySelector('.givaway');
 const deadline = document.querySelector('.deadline');
-const items = document.querySelectorAll('.deadline-format');
+const items = document.querySelectorAll('.deadline-format h4');
 
 let tempDate =  new Date();
 let tempYear = tempDate.getFullYear();
-let tempMonth = document.querySelectorAll('.deadline-format h4')
+let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 //months are zero index based;
 const futureDate = new Date(tempYear, tempMonth,tempDay + 10, 11, 30, 0);
@@ -66,4 +66,22 @@ const oneMinute = 60 * 1000;
 
 let days = t / oneDay;
 days = Math.floor(days)
-let hours = Math.floor((t % oneDay) / oneHour)
+let hours = Math.floor((t % oneDay) / oneHour);
+let minutes = Math.floor((t % oneHour) / oneMinute);
+let seconds = Math.floor((t % oneMinute) / 1000);
+
+// set values array
+
+const values = [days, hours, minutes, seconds];
+function format(item) {
+    if (item < 10) {
+        return (item = `0${item}`)
+    }
+    return item
+}
+
+items.forEach(function (item, index) {
+    item.innerHTML =  format(values[index]);
+});
+
+
